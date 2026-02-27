fa67dd5be17d62a9a7316114899fecae43505dbc
David Chandler <contact@dechandler.io>
2026-02-27 03:56:05 -0800

    surface markyaml loader and cli preprocessor

--- declib/__init__.py
+++ declib/__init__.py
@@ -3,8 +3,12 @@ from .api import DeclibApi
 
 from .cli import DeclibCli
 
+from .cli_preprocessor import DeclibCliPreprocessor
+
 from .config import DeclibConfig
 
 from .logging import DeclibLogger
 
 from .main import DeclibMain
+
+from .utils import DeepAddressableDictlike, MarkYamlDataLoader
